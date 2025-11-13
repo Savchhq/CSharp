f8e8d7916cb43e30ca4883a8ecdc0fe116ec42c4
Savchhq <vladsavchuk7002@gmail.com>
2025-11-13 14:54:31 +0200

    Simple Calculator

---
+++ Study/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.Clear();
+Console.Write("Enter first number: ");
+string Input = Console.ReadLine()!;
+float firstNum = float.Parse(Input);
+Console.Write("Enter second number: ");
+Input = Console.ReadLine()!;
+float secondNum = float.Parse(Input);
+System.Console.Write("Change a arithmetic operation (+, -, *, /): ");
+string operation = Console.ReadLine()!;
+
+if (operation == "+")
+{
+    System.Console.WriteLine($"Result: {firstNum + secondNum}");
+}
+else if (operation == "-")
+{
+    System.Console.WriteLine($"Result: {firstNum - secondNum}");
+}
+else if (operation == "*")
+{
+    System.Console.WriteLine($"Result: {firstNum * secondNum}");
+}
+else if (operation == "/")
+{
+    if(secondNum != 0)
+    System.Console.WriteLine($"Result: {firstNum / secondNum}");
+    else
+    {
+        System.Console.WriteLine("Error! Division on null");
+    }
+}
+else
+{
+    System.Console.WriteLine("Error! Incorrect operation");
+}
+Input = Console.ReadLine()!;
+float a = float.Parse(Input);
+float b = float.Parse(Console.ReadLine());
