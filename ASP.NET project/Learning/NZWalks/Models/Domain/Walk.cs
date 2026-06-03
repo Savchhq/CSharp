c4228940b55b92751d51b90b2df0b39528fd8f77
Savchhq <vladsavchuk7002@gmail.com>
2026-06-04 00:12:46 +0300

    create domain models

---
+++ ASP.NET project/Learning/NZWalks/Models/Domain/Walk.cs
@@ -0,0 +1,17 @@
+namespace NZWalks.Models.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double LengthInKM { get; set; }
+        public string? WalkImageUrl { get; set; }
+        public Guid RegionId { get; set; }
+        public Guid DifficultyId { get; set; }
+
+        public Region Region { get; set; }
+        public Difficulty Difficulty { get; set; }
+
+    }
+}
