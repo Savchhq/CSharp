e4a888a9bc5dcd981d91e35db729ccfef4ea91df
Savchhq <vladsavchuk7002@gmail.com>
2026-06-23 17:05:12 +0300

    feat: Create dbContext

---
+++ TodoApp/TodoApp.DAL/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApp.Core.Models;
+namespace NZWalks.Data
+{
+    public class TodoAppDbContext : DbContext
+    {
+        public TodoAppDbContext(DbContextOptions dbContextOptions): base( dbContextOptions)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<TodoTask> TodoTasks { get; set; } 
+    }
+}
