3560ab5eade71f134b4810fc37cffd2580a26bea
Savchhq <vladsavchuk7002@gmail.com>
2026-06-24 21:06:35 +0300

    chore: Create migrations

--- TodoApp/TodoApp.DAL/Data/AppDbContext.cs
+++ TodoApp/TodoApp.DAL/Data/AppDbContext.cs
@@ -1,6 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using TodoApp.Core.Models;
-namespace NZWalks.Data
+namespace TodoApp.DAL.Data
 {
     public class TodoAppDbContext : DbContext
     {
@@ -11,6 +11,29 @@ namespace NZWalks.Data
 
         public DbSet<User> Users { get; set; }
         public DbSet<Category> Categories { get; set; }
-        public DbSet<TodoTask> TodoTasks { get; set; } // need to create migration, connect db
-    }
+        public DbSet<TodoTask> TodoTasks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<TodoTask>()
+                .HasOne(t => t.Category)
+                .WithMany(c => c.Tasks)
+                .HasForeignKey(t => t.CategoryId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<TodoTask>()
+                .HasOne(t => t.User)
+                .WithMany(u => u.Tasks)
+                .HasForeignKey(t => t.UserId)
+                .OnDelete(DeleteBehavior.NoAction); 
+
+            modelBuilder.Entity<Category>()
+                .HasOne(c => c.User)
+                .WithMany(u => u.Categories)
+                .HasForeignKey(c => c.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }        
 }
